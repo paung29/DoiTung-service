8f8e1e12d976bd8c3195d314cc0d52b79da2d68e
Uni-T17 <mintayzamt17@gmail.com>
2026-06-02 13:44:58 +0700

    Fix route path for retrieving all cluster forms by zone: update endpoint to use consistent naming

--- internal/modules/cluster/cluster.route.go
+++ internal/modules/cluster/cluster.route.go
@@ -14,5 +14,5 @@ func ClusterRoutes(app *fiber.App, handler *ClusterHandler) {
 	cluster.Get("/get-cluster-form", middleware.RequiredAuth, handler.GetClusterForm)
 	cluster.Put("/update-cluster-form", middleware.RequiredAuth, handler.UpdateClusterForm)
 	cluster.Get("/get-cluster-form-histories", middleware.RequiredAuth, handler.GetClusterFormHistories)
-	cluster.Get("/get-all-cluster-form-by-zone", middleware.RequiredAuth, middleware.RequireRoles("ADMIN"), handler.GetAllClustersFormByZone)
+	cluster.Get("/get-cluster-forms-by-zone", middleware.RequiredAuth, middleware.RequireRoles("ADMIN"), handler.GetAllClustersFormByZone)
 }
